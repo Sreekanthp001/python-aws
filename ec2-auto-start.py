7cf2e6d0bfed97550f6c89980d0efafbd9755db6
Sreekanthp001 <sreekanthpaleti1999@gmail.com>
2025-09-13 18:10:44 +0530

    python-aws-lambda

--- ec2-auto-start.py
+++ ec2-auto-start.py
@@ -1,38 +1,38 @@
-# import boto3
-# from botocore.exceptions import ClientError
+import boto3
+from botocore.exceptions import ClientError
 
-# def start_instances_by_tag(tags, region):
-#     ec2 = boto3.client('ec2', region_name=region)
-#     #modify tag_filters to look for stopped instances
-#     tag_filters = [{ 'Name': f'tag:{key}', 'Values': [value]} for key, value in tags.items()]
-#     tag_filters.append({'Name': 'instance-state-name', 'Values': ['stopped']}) # include only stopped instances
+def start_instances_by_tag(tags, region):
+    ec2 = boto3.client('ec2', region_name=region)
+    #modify tag_filters to look for stopped instances
+    tag_filters = [{ 'Name': f'tag:{key}', 'Values': [value]} for key, value in tags.items()]
+    tag_filters.append({'Name': 'instance-state-name', 'Values': ['stopped']}) # include only stopped instances
 
-#     try:
-#         paginator = ec2.get_paginator('describe_instances')
-#         page_iterator = paginator.paginate(Filters=tag_filters)
+    try:
+        paginator = ec2.get_paginator('describe_instances')
+        page_iterator = paginator.paginate(Filters=tag_filters)
 
-#         instances = []
-#         for page in page_iterator:
-#             for reservation in page['Reservations']:
-#                 for instance in reservation['Instances']:
-#                     instance.append(instance['InstanceId'])
+        instances = []
+        for page in page_iterator:
+            for reservation in page['Reservations']:
+                for instance in reservation['Instances']:
+                    instance.append(instance['InstanceId'])
 
         
-#         if instances:
-#             # use start_instances insted of stop_instances
-#             start_response = ec2.start_instances(InstanceIds=instances)
-#             print(f'Started instances: {instances}')
-#             return start_response
-#         else:
-#             print('No stopped instances found with the specified tags.')
-#     except ClientError as e:
-#         print(f'An error occurred: {e}')
+        if instances:
+            # use start_instances insted of stop_instances
+            start_response = ec2.start_instances(InstanceIds=instances)
+            print(f'Started instances: {instances}')
+            return start_response
+        else:
+            print('No stopped instances found with the specified tags.')
+    except ClientError as e:
+        print(f'An error occurred: {e}')
 
-# def lambda_handler(event, context):
-#     # specify the tag key, value, and region
-#     tags = {
-#         'AutoStartStop': 'True',
-#         'Environment': 'Dev' #example additional tag
-#     }
-#     region = 'us-east-1'
-#     start_instances_by_tag(tags, region)
+def lambda_handler(event, context):
+    # specify the tag key, value, and region
+    tags = {
+        'AutoStartStop': 'True',
+        'Environment': 'Dev' #example additional tag
+    }
+    region = 'us-east-1'
+    start_instances_by_tag(tags, region)
--- lambda.tf
+++ lambda.tf
@@ -1,7 +1,26 @@
+data "archive_file" "ec2_auto_start_zip" {
+    type        = "zip"
+    source_file = "${path.module}/ec2-auto-start.py"
+    output_path = "${path.module}/ec2_auto_start_zip"
+}
+
 data "archive_file" "ec2_auto_stop_zip" {
     type         = "zip"
     source_file  = "${path.module}/ec2-auto-stop.py"
     output_path  = "${path.module}/ec2_auto_stop_zip"
+}
+
+resource "aws_lambda_function" "ec2_auto_start" {
+    function_name  = "EC2AutoStart"
+    handler        = "ec2-auto-start.lambda_handler"
+    role           = aws_iam_role.lambda_role.arn
+    runtime        = "python3.12"
+
+    filename         = data.archive_file.ec2_auto_start_zip.output_path
+    source_code_hash = data.archive_file.ec2_auto_start_zip.output_base64sha256
+    timeout = 60
+
+    depends_on = [aws_iam_policy.lambda_policy]
 }
 
 resource "aws_lambda_function" "ec2_auto_stop" {
